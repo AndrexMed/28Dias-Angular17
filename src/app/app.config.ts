ef7aa403582997ca842ba4e20dd8e786b7dbd52b
Giovanni Andres Alzate Agudelo <119427037+AndrexMed@users.noreply.github.com>
2024-04-14 21:25:35 -0500

    Hidratación NO destructiva Angular 17 con SSG (Hydration graduating)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withViewTransitions,
+} from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withHttpTransferCacheOptions,
+  withNoHttpTransferCache,
+} from '@angular/platform-browser';
 import {
   provideHttpClient,
   withFetch,
@@ -13,7 +21,12 @@ import { ErrorResponseInterceptor } from './shared/error-response-interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
-    provideClientHydration(),
+    provideClientHydration(
+      //withNoHttpTransferCache(), //Desactivar la cache em las peticiones http, hace que se repitan las solicitudes
+      withHttpTransferCacheOptions({
+        includePostRequests: true,
+      })
+    ),
     provideHttpClient(
       withFetch(),
       withInterceptors([ErrorResponseInterceptor])
