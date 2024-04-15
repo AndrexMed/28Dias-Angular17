339384450abb3af4bcc11919d52d6329b1a6b07b
Giovanni Andres Alzate Agudelo <119427037+AndrexMed@users.noreply.github.com>
2024-04-14 19:16:28 -0500

    ¿Cómo arreglar el error de runininjectioncontext?

--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -1,8 +1,15 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  EnvironmentInjector,
+  OnInit,
+  inject,
+  runInInjectionContext,
+} from '@angular/core';
 import { ProductService } from '../../api/product.service';
 import { Product } from '../../models/product.model';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { ProductComponent } from './product/product.component';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-products',
@@ -11,8 +18,18 @@ import { ProductComponent } from './product/product.component';
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
   productSvc = inject(ProductService);
-
   products$ = this.productSvc.getAllProducts();
+
+  productSvc2!: any;
+  private readonly injector = inject(EnvironmentInjector);
+
+  ngOnInit(): void {
+    runInInjectionContext(this.injector, () => {
+      this.productSvc2 = inject(ProductService);
+      const result = toSignal(this.products$);
+      console.log(result);
+    });
+  }
 }
