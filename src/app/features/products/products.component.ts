0dd271c277967c9b015f8a1feb92da3980ef4f06
Giovanni Andres Alzate Agudelo <119427037+AndrexMed@users.noreply.github.com>
2024-04-16 20:01:24 -0500

    Angular 17 DEFER - Deferrable views ¿Cómo utilizarla?

--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -10,11 +10,13 @@ import { Product } from '../../models/product.model';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { ProductComponent } from './product/product.component';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { SkeletonComponent } from '../../shared/components/skeleton/skeleton.component';
+import { LoadingComponent } from '../../shared/components/loading/loading.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [ProductComponent, AsyncPipe],
+  imports: [ProductComponent, AsyncPipe, SkeletonComponent, LoadingComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
@@ -24,6 +26,8 @@ export class ProductsComponent implements OnInit {
 
   productSvc2!: any;
   private readonly injector = inject(EnvironmentInjector);
+
+  cond: boolean = false;
   
   ngOnInit(): void {
     // runInInjectionContext(this.injector, () => {
