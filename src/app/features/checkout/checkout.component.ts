78991dbf696ddbdd374e8c5ea1660d994c629f3a
Giovanni Andres Alzate Agudelo <155462394+GiovanniAndresAlzateAgudelo@users.noreply.github.com>
2024-05-25 22:31:45 -0500

    checkout - pagos pendiente

--- src/app/features/checkout/checkout.component.ts
+++ src/app/features/checkout/checkout.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartStore2 } from 'app/store/shopping-cart.store';
+import { CheckoutService } from './services/checkout.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.scss'
+  styleUrl: './checkout.component.scss',
 })
 export default class CheckoutComponent {
+  cartStore = inject(CartStore2);
 
+  private readonly _checkoutSvc = inject(CheckoutService);
+
+  onProceedToPay(): void {
+    //this._checkoutSvc.onProceedToPay(this.cartStore.products());
+  }
+
+  removeItem(id: number): void {
+    this.cartStore.removeFromCart(id);
+  }
+
+  clearAll(): void {
+    this.cartStore.clearCart();
+  }
 }
